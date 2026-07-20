b79d70acc697e20680334d96be2a23cf59a22c99
Shabana <shabanashajahan052003@gmail.com>
2026-07-20 13:27:30 +0400

    Use Django URL tags

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -29,11 +29,8 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    ".onrender.com",
-]
+ALLOWED_HOSTS = ["*"]
+
 
 # Application definition
 
